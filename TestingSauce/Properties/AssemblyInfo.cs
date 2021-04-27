8e1e1003dda30eccc8cf9037fd7e88d3d3c9f69c
echarly <43246364+echarly@users.noreply.github.com>
2021-04-26 21:39:10 -0500

    Add files via upload

---
+++ TestingSauce/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestingSauce")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestingSauce")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("18ade0b1-1a05-4908-8371-beec90f4b6ea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestingSauce/UnitTest1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SauceDemo;
+using SauceDemo.PageObjects;
+
+namespace TestingSauce
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // Before tests
+        LoginPage login = new LoginPage();
+        UserInformation user = new UserInformation();
+        HomePage home = new HomePage();
+        BasePage basePage = new BasePage();
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            basePage
+                .InitializeDriver()
+                .OpenBrowser();
+        }
+
+        [TestMethod]
+        public void ValidUserTest()
+        {
+            user.Valid_UserInformation();
+
+            login.UserLogin(user.UserEmail, user.UserPassword);
+            Assert.AreEqual(basePage.ObtainText(home.ProductsTitle), basePage.ProductsTitleText);
+        }
+
+        [TestMethod]
+        public void InValidUserTest()
+        {
+            user.InValid_UserInformation();
+
+            login.UserLogin(user.UserEmail, user.UserPassword);
+            Assert.AreEqual(basePage.ObtainText(login.ErrorMessageTitle), basePage.ErrorMessageText);
+        }
+
+        [TestMethod]
+        public void ValidUserTest_Logout()
+        {
+            user.Valid_UserInformation();
+
+            login.UserLogin(user.UserEmail, user.UserPassword);
+            Assert.AreEqual(basePage.ObtainText(home.ProductsTitle), basePage.ProductsTitleText);
+
+            home.LogOut();
+            
+            Assert.AreEqual(basePage.ElementDisplayed(login.UserEmail), true);
+        }
+
+        [TestMethod]
+        public void SortProducts()
+        {
+            user.Valid_UserInformation();
+
+            login.UserLogin(user.UserEmail, user.UserPassword);
+            Assert.AreEqual(basePage.ObtainText(home.ProductsTitle), basePage.ProductsTitleText);
+
+            home.SelectDropDown(SortContainer.LowtoHigh);
+        }
+
+        [TestMethod]
+        public void AddMultpleItemsToShoppingCart()
+        {
+            user.Valid_UserInformation();
+
+            login.UserLogin(user.UserEmail, user.UserPassword);
+            Assert.AreEqual(basePage.ObtainText(home.ProductsTitle), basePage.ProductsTitleText);
+
+            home.AddToShoppingCart(home.SauceLabsBackPack);
+            Assert.AreEqual(basePage.ObtainText(home.RemoveSauceLabsBackPack), "REMOVE");
+
+            home.AddToShoppingCart(home.SauceLabsBikeLight);
+            Assert.AreEqual(basePage.ObtainText(home.RemoveSauceLabsBikeLight), "REMOVE");
+
+            home.AddToShoppingCart(home.SauceLabsOnesies);
+            Assert.AreEqual(basePage.ObtainText(home.RemoveSauceLabsOnesies), "REMOVE");
+
+            home.AddToShoppingCart(home.SauceLabsTShirtRed);
+            Assert.AreEqual(basePage.ObtainText(home.RemoveSauceLabsTShirtRed), "REMOVE");
+        }
+
+        [TestMethod]
+        public void AddProductToShoppingCart()
+        {
+            user.Valid_UserInformation();
+
+            login.UserLogin(user.UserEmail, user.UserPassword);
+            Assert.AreEqual(basePage.ObtainText(home.ProductsTitle), basePage.ProductsTitleText);
+
+            home.SelectDropDown(SortContainer.LowtoHigh);
+
+            basePage.WaitTime(home.SauceLabsOnesies);
+            home.AddToShoppingCart(home.SauceLabsOnesies);
+
+            basePage.WaitTime(home.RemoveSauceLabsOnesies);
+            Assert.AreEqual(basePage.ObtainText(home.RemoveSauceLabsOnesies), "REMOVE");
+
+            home.OpenShoppingCart();
+            Assert.AreEqual(basePage.ObtainText(home.InventoryItemName), home.Itemtitle);
+        }
+
+        [TestMethod]
+        public void CheckOutProduct()
+        {
+            user.Valid_UserInformation();
+
+            login.UserLogin(user.UserEmail, user.UserPassword);
+            Assert.AreEqual(basePage.ObtainText(home.ProductsTitle), basePage.ProductsTitleText);
+
+            home.SelectDropDown(SortContainer.LowtoHigh);
+
+            basePage.WaitTime(home.SauceLabsOnesies);
+            home.AddToShoppingCart(home.SauceLabsOnesies);
+
+            basePage.WaitTime(home.RemoveSauceLabsOnesies);
+            Assert.AreEqual(basePage.ObtainText(home.RemoveSauceLabsOnesies), "REMOVE");
+
+            home.OpenShoppingCart();
+            Assert.AreEqual(basePage.ObtainText(home.InventoryItemName), home.Itemtitle);
+
+            home.CheckOutProduct();
+            basePage.WaitTime(home.PaymentInformation);
+            Assert.IsNotNull(basePage.ObtainText(home.PaymentInformation));
+        }
+
+        [TestCleanup]
+        public void TestClose()
+        {
+            basePage.CloseDriver();
+        }
+    }
+}
